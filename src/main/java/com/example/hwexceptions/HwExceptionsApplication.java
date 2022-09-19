4f85aa789cc626513d67ed827703d636b795309d
Daria Gromova <igromova81@gmail.com>
2022-09-19 12:31:07 +0300

    Initial commit

---
+++ src/main/java/com/example/hwexceptions/HwExceptionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.hwexceptions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwExceptionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hwexceptions/HwExceptionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hwexceptions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwExceptionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
