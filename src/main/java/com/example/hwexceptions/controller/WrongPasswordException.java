a7e21cbd0efac29598036fe21c9af900d3b5738d
Daria Gromova <igromova81@gmail.com>
2022-10-05 21:50:23 +0300

    HW2

--- src/main/java/com/example/hwexceptions/controller/WrongPasswordException.java
+++ src/main/java/com/example/hwexceptions/controller/WrongPasswordException.java
@@ -1,5 +1,9 @@
 package com.example.hwexceptions.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class WrongPasswordException extends RuntimeException {
     public WrongPasswordException(String message) {
         super(message);
--- src/main/java/com/example/hwexceptions/controller/exceptions/WrongLoginException.java
+++ src/main/java/com/example/hwexceptions/controller/exceptions/WrongLoginException.java
@@ -1,5 +1,9 @@
 package com.example.hwexceptions.controller.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class WrongLoginException extends RuntimeException {
     public WrongLoginException(String message) {
         super(message);
